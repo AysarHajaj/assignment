6f2d82f1066de54c5ed9ac51de69ef02fb86bf14
aysar <aysarhajaj@gmail.com>
2022-10-02 16:01:53 +0300

    add getRedundantNumbers function

--- challenge_two_three_four/app/Http/Controllers/ChallengeTwoController.php
+++ challenge_two_three_four/app/Http/Controllers/ChallengeTwoController.php
@@ -6,4 +6,25 @@ use Illuminate\Http\Request;
 
 class ChallengeTwoController extends Controller
 {
+    public function getRedundantNumbers(Request $request)
+    {
+        $numbers = $request->input('numbers');;
+        $data = [];
+        foreach ($numbers as $number) {
+            if (isset($data[$number])) {
+                $data[$number] = $data[$number] + 1;
+            } else {
+                $data[$number] = 1;
+            }
+        }
+
+        $result = "";
+        foreach ($data as $key => $value) {
+            if ($value > 1) {
+                $result = $result . $key . " ";
+            }
+        }
+
+        return $result;
+    }
 }

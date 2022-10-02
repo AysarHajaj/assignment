cebbab0e718d8eb4eacc0f402c456c5f061bc155
aysar <aysarhajaj@gmail.com>
2022-10-02 15:50:01 +0300

    import controller in api route

--- challenge_one_backend/routes/api.php
+++ challenge_one_backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------

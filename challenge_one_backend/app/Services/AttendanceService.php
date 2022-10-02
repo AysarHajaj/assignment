5b0d0203671813cda1ce7c486f7e6b1ca77191a8
aysar <aysarhajaj@gmail.com>
2022-10-02 15:31:31 +0300

    implement uploadContent function

--- challenge_one_backend/app/Services/AttendanceService.php
+++ challenge_one_backend/app/Services/AttendanceService.php
@@ -2,11 +2,33 @@
 
 namespace App\Services;
 
+use excel;
+
 
 class AttendanceService
 {
     public function uploadContent($file)
     {
+        if ($file) {
+            $filename = $file->getClientOriginalName();
+            $extension = $file->getClientOriginalExtension(); //Get extension of uploaded file
+            $tempPath = $file->getRealPath();
+            $fileSize = $file->getSize(); //Get size of uploaded file in bytes
+            //Check for file extension and size
+            $this->checkUploadedFileProperties($extension, $fileSize);
+            //Where uploaded file will be stored on the server 
+            $location = 'uploads'; //Created an "uploads" folder for that
+            // Upload file
+            $file->move($location, $filename);
+            // In case the uploaded file path is to be stored in the database 
+            $filepath = public_path($location . "/" . $filename);
+
+            Excel::import(new ImportAttendance, $filepath);
+            return true;
+        } else {
+            //no file was uploaded
+            throw new \Exception('No file was uploaded', Response::HTTP_BAD_REQUEST);
+        }
     }
 
     public function checkUploadedFileProperties($extension, $fileSize)

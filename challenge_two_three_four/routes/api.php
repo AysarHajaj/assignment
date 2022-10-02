805c8945b76cf3bfe76d2bf268ccdaf82fb222ac
aysar <aysarhajaj@gmail.com>
2022-10-02 16:08:45 +0300

    add get data group by  company name route  route

--- challenge_two_three_four/routes/api.php
+++ challenge_two_three_four/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChallengeFourController;
 use App\Http\Controllers\ChallengeTwoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/get_redundant_data', [ChallengeTwoController::class, "getRedundantNumbers"]);
+Route::post('/get_group_by_company_name', [ChallengeFourController::class, "groupByCompanyName"]);

b3cc78cfcf22671edbe3f705c27ffd2c24ac4f23
aysar <aysarhajaj@gmail.com>
2022-10-02 15:29:39 +0300

    inject attendace service and add upload content function

--- challenge_one_backend/app/Http/Controllers/AttendanceController.php
+++ challenge_one_backend/app/Http/Controllers/AttendanceController.php
@@ -2,8 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AttendanceService;
 use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
+    function __construct(AttendanceService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function uploadContent(Request $request)
+    {
+        $file = $request->file('uploaded_file');
+
+        return $this->service->uploadContent($file);
+    }
 }

468ed06988017e6b03e6afc77a934399d0ca3134
aysar <aysarhajaj@gmail.com>
2022-10-02 15:20:57 +0300

    add schedule model with its  relation

---
+++ challenge_one_backend/app/Models/Schedule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    use HasFactory;
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function shift()
+    {
+        return $this->belongsTo(Shift::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function attendance()
+    {
+        return $this->hasOne(Attendance::class, 'schedule_id');
+    }
+}

0b0416734368e971b425d5bba42904182c229135
aysar <aysarhajaj@gmail.com>
2022-10-02 15:16:29 +0300

    add attendace table migration

---
+++ challenge_one_backend/database/migrations/2022_10_02_073059_create_attendances_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained('employees');
+            $table->foreignId('schedule_id')->constrained('schedules');
+            $table->time('check_in')->nullable();
+            $table->time('check_out')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            $table->dropForeign(['employee_id']);
+            $table->dropForeign(['schedule_id']);
+        });
+        Schema::dropIfExists('attendances');
+    }
+}

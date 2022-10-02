0c1a3fae3acd68523560329ba453e2dae6ff2260
aysar <aysarhajaj@gmail.com>
2022-10-02 15:07:46 +0300

    implement app component

--- challenge_one_frontend/src/App.tsx
+++ challenge_one_frontend/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Table from './AttendanceTable/Table';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`http://127.0.0.1:8000/api/test`).then((res) => {
+      return res.json();
+    }).then((resData) => {
+      setData(resData);
+      setLoading(false);
+    })
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <div>Loading...</div>}
+      <Table  data={data}/>
     </div>
   );
 }

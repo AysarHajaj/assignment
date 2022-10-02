6616c01686a8157f11ac2d478eaa82de41ca41a3
aysar <aysarhajaj@gmail.com>
2022-10-02 15:23:37 +0300

    add some testing data in the seeds

--- challenge_one_backend/database/seeders/DatabaseSeeder.php
+++ challenge_one_backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
+use App\Models\Location;
+use App\Models\Schedule;
+use App\Models\Shift;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +17,82 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Employee::insert([
+            ["name" => "Aysar"],
+            ["name" => "Jhon"],
+            ["name" => "Ahmed"],
+        ]);
+
+        Location::insert([
+            ["name" => "x"],
+            ["name" => "y"],
+            ["name" => "z"],
+        ]);
+
+        Shift::insert([
+            [
+                "name" => "AM",
+                "start_at" => "8:00",
+                "end_at" => "14:00",
+            ],
+            [
+                "name" => "PM",
+                "start_at" => "15:00",
+                "end_at" => "22:00",
+            ],
+        ]);
+
+        Schedule::insert([
+            [
+                "employee_id" => 1,
+                "shift_id" => 1,
+                "location_id" => 1
+            ],
+            [
+                "employee_id" => 1,
+                "shift_id" => 1,
+                "location_id" => 2
+            ],
+            [
+                "employee_id" => 1,
+                "shift_id" => 2,
+                "location_id" => 1
+            ],
+            [
+                "employee_id" => 2,
+                "shift_id" => 1,
+                "location_id" => 1
+            ],
+            [
+                "employee_id" => 2,
+                "shift_id" => 2,
+                "location_id" => 2
+            ],
+            [
+                "employee_id" => 2,
+                "shift_id" => 2,
+                "location_id" => 1
+            ],
+            [
+                "employee_id" => 3,
+                "shift_id" => 1,
+                "location_id" => 1
+            ],
+            [
+                "employee_id" => 3,
+                "shift_id" => 1,
+                "location_id" => 2
+            ],
+            [
+                "employee_id" => 3,
+                "shift_id" => 2,
+                "location_id" => 1
+            ],
+            [
+                "employee_id" => 3,
+                "shift_id" => 2,
+                "location_id" => 2
+            ],
+        ]);
     }
 }

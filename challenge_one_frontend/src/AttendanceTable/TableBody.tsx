b85823df582e10531ed70b60946fda2a9eea0ca7
aysar <aysarhajaj@gmail.com>
2022-10-02 15:02:49 +0300

    add table body component

---
+++ challenge_one_frontend/src/AttendanceTable/TableBody.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Employee from '../Interfaces/Employee';
+
+function TableBody({data}: {data: Array<Employee>}) {
+  
+  return (
+    <div></div>
+  );
+}
+
+export default TableBody;

df5001a4e9159a370ef881b7274d1f88f29d1caa
aysar <aysarhajaj@gmail.com>
2022-10-02 15:03:37 +0300

    add getScheduleCells

--- challenge_one_frontend/src/AttendanceTable/TableBody.tsx
+++ challenge_one_frontend/src/AttendanceTable/TableBody.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Employee from '../Interfaces/Employee';
+import Schedule from '../Interfaces/Schedule';
 
 function TableBody({data}: {data: Array<Employee>}) {
   
+  const getScheduleCells = (schedule: Schedule) => <React.Fragment key={schedule.id}>
+  <td>{schedule.shift.name}</td>
+  <td>{schedule.shift.start_time}</td>
+  <td>{schedule.shift.end_time}</td>
+  <td>{schedule.attendance.check_in}</td>
+  <td>{schedule.attendance.check_out}</td>
+  <td>{schedule.attendance.working_hours}</td>
+</React.Fragment>
+
   return (
     <div></div>
   );
